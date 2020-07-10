5ddc757901773d37775685d736bd36b2cac72a3c
Hasan-2020 <tauhidhasan11@gmail.com>
2020-07-10 20:22:55 +0300

    Updates

---
+++ precache-manifest.1b2d5efcb2b8faa2e566852701ad100b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91d45e1aff52dcde89c7dc95ff43d7ea",
+    "url": "/todohasan/index.html"
+  },
+  {
+    "revision": "096f6c588ea5918ec8f4",
+    "url": "/todohasan/static/css/main.6bda9865.chunk.css"
+  },
+  {
+    "revision": "18151e03f9bd1457916d",
+    "url": "/todohasan/static/js/2.a248f6da.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todohasan/static/js/2.a248f6da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "096f6c588ea5918ec8f4",
+    "url": "/todohasan/static/js/main.548f5c33.chunk.js"
+  },
+  {
+    "revision": "2c38dff8737d7c760e98",
+    "url": "/todohasan/static/js/runtime-main.3ab9594f.js"
+  }
+]);
